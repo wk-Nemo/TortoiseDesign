8e3c3f1a544e0526b9d07ba87f853bb29d824219
wk-Nemo <2181544173@qq.com>
2021-10-06 11:48:19 +0800

    alert init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,52 @@
 import React from 'react'; 
-import Button, {ButtonType, ButtonSize} from './components/Button/button';
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
+import Alert, { AlertType } from './components/Alert/alert';
 
 function App() {
   return (
     <div className="App">
-      <Button className="hello" onClick={(e) => {e.preventDefault(); alert(123)}}>Hello</Button>
-      <Button
-        disabled={true}
-      >Disabled Button</Button>
-      <Button
-        btnType={ButtonType.Primary}
-        size={ButtonSize.Large}
-      >Large Primary</Button>
-      <Button
-        btnType={ButtonType.Danger}
-        size={ButtonSize.Small}
-      >small Danger</Button>
-      <Button
-        btnType={ButtonType.Link}
-        href="http://www.baidu.com"
-      >BaiDu Link</Button>
-      <Button
-        btnType={ButtonType.Link}
-        disabled={true}
-        href="http://www.baidu.com"
-      >Disabled Link</Button>
+      <div className="item buttons">
+        <Button className="hello" onClick={(e) => {e.preventDefault(); alert(123)}}>Hello</Button>
+        <Button
+          disabled={true}
+        >Disabled Button</Button>
+        <Button
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Large}
+        >Large Primary</Button>
+        <Button
+          btnType={ButtonType.Danger}
+          size={ButtonSize.Small}
+        >small Danger</Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="http://www.baidu.com"
+        >BaiDu Link</Button>
+        <Button
+          btnType={ButtonType.Link}
+          disabled={true}
+          href="http://www.baidu.com"
+        >Disabled Link</Button>
+      </div>
+      <div className="item alerts">
+        <Alert
+          closeable={true}
+          message="default Alert"
+          description="this is a default Alert"
+        ></Alert>
+        <Alert
+          message="success Alert"
+          type={AlertType.success}
+        ></Alert>
+        <Alert
+          message="danger Alert"
+          type={AlertType.danger}
+        ></Alert>
+        <Alert
+          message="warning Alert"
+          type={AlertType.warning}
+        ></Alert>
+      </div>
     </div>
   );
 }
