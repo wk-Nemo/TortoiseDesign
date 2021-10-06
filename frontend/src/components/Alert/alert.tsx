57c3195de0174637f3f7bb1d658b9e5ce62c9d89
wk-Nemo <2181544173@qq.com>
2021-10-06 16:34:14 +0800

    transition finish

--- frontend/src/components/Alert/alert.tsx
+++ frontend/src/components/Alert/alert.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import classNames from "classnames";
 import Icon from '../Icon/icon'
+import Transition from '../Transition/transition'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
@@ -47,21 +48,26 @@ const Alert: React.FC<BaseAlertProps> = (props) => {
   }
 
   return (
-    <div className={classes} style={{display: hide ? 'none' : ''}}>
-      <div className="alert-content">
-        <div className="alert-content-message">{message}</div>
-        <div className="alert-content-description">{description}</div>
+    <Transition
+      in={!hide}
+      timeout={600}
+      animation="zoom-in-top"
+    >
+      <div className={classes}>
+        <div className="alert-content">
+          <div className="alert-content-message">{message}</div>
+          <div className="alert-content-description">{description}</div>
+        </div>
+        { closeable &&
+          <button
+            className="alert-close-icon"
+            onClick={handleClose}
+          >
+            <Icon icon="times" theme="dark" size="1x"></Icon>
+          </button>
+        }
       </div>
-      { closeable &&
-        <button
-          className="alert-close-icon"
-          onClick={handleClose}
-        >
-          <Icon icon="times" theme="dark" size="1x"></Icon>
-          {/* <svg viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg> */}
-        </button>
-      }
-    </div>
+    </Transition>
   )
 }
 
